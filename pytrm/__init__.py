2ef9ff6fc7b99fca72cddfe6b39b2c97545efe3f
Aleksey Odinokov <AlekseyOdi@yandex.ru>
2025-02-28 23:48:45 +1000

    pyright library interface fix

--- pytrm/__init__.py
+++ pytrm/__init__.py
@@ -3,22 +3,22 @@ PyTRM is a transaction manager as abstraction of database access.
 """
 
 from . import exceptions, impls
-from .configurations import configurate, get_configurated_reg
-from .decorators import transactional, transactional_with
-from .share import (
-    Context,
-    ContextManager,
-    Key,
-    NativeTransaction,
-    Propagation,
-    Registry,
-    Settings,
-    SettingsID,
-    Transaction,
-    TransactionManager,
-    find_native_transaction,
-    get_native_transaction,
-)
+from .configurations import configurate as configurate
+from .configurations import get_configurated_reg as get_configurated_reg
+from .decorators import transactional as transactional
+from .decorators import transactional_with as transactional_with
+from .share import Context as Context
+from .share import ContextManager as ContextManager
+from .share import Key as Key
+from .share import NativeTransaction as NativeTransaction
+from .share import Propagation as Propagation
+from .share import Registry as Registry
+from .share import Settings as Settings
+from .share import SettingsID as SettingsID
+from .share import Transaction as Transaction
+from .share import TransactionManager as TransactionManager
+from .share import find_native_transaction as find_native_transaction
+from .share import get_native_transaction as get_native_transaction
 
 __version__ = "0.1.0"
 __date__ = "2024-12-31"
